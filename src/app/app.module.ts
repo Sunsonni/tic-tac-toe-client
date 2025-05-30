9c6eabfbc576cc68da9eeeb512d58578ab3ebfa6
Sonnie Nguyen <sonnienguyen@mbpro-intsonnie.ssi.com>
2025-05-30 10:23:56 -0500

    starter items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GridComponent } from './grid/grid.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+      AppComponent,
+      GridComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
---
+++ src/app/service/api-fetch.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+
+@Injectable()
+export class ApiFetchService {
+
+constructor() { }
+
+// async function getData() {
+//     const url = 'http://localhost:3000/docs';
+//     try {
+//         const response = await fetch(url);
+//         if (response.status == 200){
+//             let json = response.json();
+
+//         }
+//     } catch {
+
+//     }
+// }
+
+}
